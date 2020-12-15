39ba6bcc14dbb562aa243b6478c1dbc050c3fede
Teemu Laurila <laurilateemu8@gmail.com>
2020-12-15 19:17:46 +0200

    Add support for both hands, more sensors and grab

--- rotateSerial.cs
+++ rotateSerial.cs
@@ -4,12 +4,22 @@ using System.IO;
 using System.IO.Ports;
 using UnityEngine;
 using System.Globalization;
+using Valve.VR;
 
 public class rotateSerial : MonoBehaviour
 {
     SerialPort sp = new SerialPort("COM10", 115200);
 
+    public enum handID
+    {
+        left,
+        right
+    };
+    public handID HandID;
+
     public Transform rootBone;
+
+    public SteamVR_Action_Single controllerTrigger = null;
 
     public float rotX = 0;
     public float rotY = 0;
@@ -19,97 +29,204 @@ public class rotateSerial : MonoBehaviour
     public bool bEnableOffset = false;
     public bool enableRotation = false;
 
+    private bool itemGrabbed = false;
+    private Quaternion itemOldOrientation;
+
+    private int sensorAmount = 10;
+    private int[] fingerArray = { 0, 0, 0, 0, 0, 0, 0, 0, 0, 0 };
+
     // Start is called before the first frame update
     void Start()
     {
-        sp.Open();
-        sp.ReadTimeout = 14;
+        if(controllerTrigger == null) {
+            sp.Open();
+            sp.ReadTimeout = 14;
+        }
+        
     }
 
     // Update is called once per frame
     void Update()
     {
-        if(sp.IsOpen) {
-            try{
+        if (sp.IsOpen && controllerTrigger == null)
+        {
+            try
+            {
                 serialEvent(sp);
-            }catch(System.Exception) {
+            }
+            catch (System.Exception)
+            {
                 throw;
             }
         }
 
         // If recentering orientation
-        if(Input.GetKey("c")) {
+        if (Input.GetKey("c"))
+        {
             recenter = true;
         }
+
+        updateJoints();
     }
 
     // Read serial port
     // FIXME: At the beginning the serial will time out
-    void serialEvent(SerialPort port) {
+    void serialEvent(SerialPort port)
+    {
         port.Write("f;");
         string myString = port.ReadLine();
 
-        if(myString != null) {
+        if (Input.GetKey("n"))
+        {
+            port.Write("cmin;");
+            Debug.Log(port.ReadLine());
+        }
+        if (Input.GetKey("m"))
+        {
+            port.Write("cmax;");
+            Debug.Log(port.ReadLine());
+        }
+
+        if (myString != null)
+        {
 
             // Split the string from the glove
             string[] array = myString.Split(',');
 
             // Parse the finger data
-            int finger1 = int.Parse(array[0], CultureInfo.InvariantCulture.NumberFormat);
-            int finger2 = int.Parse(array[1], CultureInfo.InvariantCulture.NumberFormat);
-            int finger3 = int.Parse(array[2], CultureInfo.InvariantCulture.NumberFormat);
+            for (int i = 0; i < sensorAmount; i++)
+            {
+                fingerArray[i] = int.Parse(array[i], CultureInfo.InvariantCulture.NumberFormat);
+            }
+        }
 
-            /*
-            float w = float.Parse(array[3], CultureInfo.InvariantCulture.NumberFormat);
-            float y = float.Parse(array[4], CultureInfo.InvariantCulture.NumberFormat);
-            float x = float.Parse(array[5], CultureInfo.InvariantCulture.NumberFormat);
-            float z = float.Parse(array[6], CultureInfo.InvariantCulture.NumberFormat);
-            
+    }
 
-            //Quaternion rotation = new Quaternion(x, y, z, w);
-            Quaternion rotation = new Quaternion(-w, -y, -z, x);
+    private void updateJoints()
+    {
+        // Override the values with trigger
+        if (controllerTrigger != null)
+        {
+            for (int i = 0; i < sensorAmount; i++)
+            {
+                if(HandID == handID.right)
+                {
+                    fingerArray[i] = (int)(controllerTrigger.GetAxis(SteamVR_Input_Sources.RightHand) * 100);
+                }
+                else
+                {
+                    fingerArray[i] = (int)(controllerTrigger.GetAxis(SteamVR_Input_Sources.LeftHand) * 100);
+                }
+                
+            }
+        }
 
-            // If recentering, save the current orientation to rotX, rotY and rotZ
-            if(recenter) {
-                rotZ = -rotation.eulerAngles.x;
-                rotY = -rotation.eulerAngles.y;
-                rotX = -rotation.eulerAngles.z;
+        // Rotate the bones
+        List<Transform> boneTransformArray = new List<Transform>();
 
-                recenter = false;
+        string HANDNESS = "l";
 
-                // Print some debug info
-                Debug.Log("Recentered orientation!");
+        string[] FINGERID =
+        {
+                "thumb",
+                "index",
+                "middle",
+                "ring",
+                "pinky"
+            };
 
-                Debug.Log("Current:");
-                Debug.Log(rotation.eulerAngles);
-                Debug.Log("New:");
-                Debug.Log(rotX);
-                Debug.Log(rotY);
-                Debug.Log(rotZ);
+
+        for (int i = 0; i < 5; i++)
+        {
+            if (i != 0)
+            {
+                boneTransformArray.Add(rootBone.Find($"wrist_l/finger_{FINGERID[i]}_meta_{HANDNESS}/finger_{FINGERID[i]}_0_{HANDNESS}").transform);
+                boneTransformArray.Add(rootBone.Find($"wrist_l/finger_{FINGERID[i]}_meta_{HANDNESS}/finger_{FINGERID[i]}_0_{HANDNESS}/finger_{FINGERID[i]}_1_{HANDNESS}").transform);
+                boneTransformArray.Add(rootBone.Find($"wrist_l/finger_{FINGERID[i]}_meta_{HANDNESS}/finger_{FINGERID[i]}_0_{HANDNESS}/finger_{FINGERID[i]}_1_{HANDNESS}/finger_{FINGERID[i]}_2_{HANDNESS}").transform);
+
+            }
+            else
+            {
+                // Thumb doesn't have finger_meta
+                boneTransformArray.Add(rootBone.Find($"wrist_l/finger_{FINGERID[i]}_0_{HANDNESS}").transform);
+                boneTransformArray.Add(rootBone.Find($"wrist_l/finger_{FINGERID[i]}_0_{HANDNESS}/finger_{FINGERID[i]}_1_{HANDNESS}").transform);
+                boneTransformArray.Add(rootBone.Find($"wrist_l/finger_{FINGERID[i]}_0_{HANDNESS}/finger_{FINGERID[i]}_1_{HANDNESS}/finger_{FINGERID[i]}_2_{HANDNESS}").transform);
             }
 
-            // Set an offset for the hand with the recenter function
-            if(rotX != 0 && rotY != 0 && rotZ != 0 && bEnableOffset == true) {
-                Quaternion offsetAngle = Quaternion.Euler(rotX, rotY, rotZ);
-                rotation = rotation * offsetAngle;
+        }
+
+        for (var i = 0; i < 5; i++)
+        {
+
+            // The first joint in the thumb needs to be rotated 90 degrees more, I have no idea why
+            if (i == 0)
+            {
+                boneTransformArray[i * 3].localEulerAngles = new Vector3(0, -fingerArray[i * 2] / 2 - 50, -fingerArray[i * 2] / 2 - 144);
             }
-            
-            if(enableRotation) {
-                // Change the objects orientation
-                transform.rotation = rotation;
+            else
+            {
+                boneTransformArray[i * 3].localEulerAngles = new Vector3(0, 0, -fingerArray[i * 2]);
             }
-            */
 
-            // Rotate the bones
+            boneTransformArray[i * 3 + 1].localEulerAngles = new Vector3(0, 0, -fingerArray[i * 2 + 1]);
+            boneTransformArray[i * 3 + 2].localEulerAngles = new Vector3(0, 0, -fingerArray[i * 2 + 1]);
+        }
 
-            // INDEX
-            rootBone.Find("wrist_l/finger_index_meta_l/finger_index_0_l").transform.localEulerAngles = new Vector3(0, 0, -finger1);
-            rootBone.Find("wrist_l/finger_index_meta_l/finger_index_0_l/finger_index_1_l").transform.localEulerAngles = new Vector3(0, 0, -finger1);
+        // Get all of the triggers
+        var indexFingerTrigger = GameObject.FindGameObjectsWithTag("FingerTrigger");
+        var bIndexColliding = indexFingerTrigger[0].GetComponent<showCollision>().colliding;
 
-            // MIDDLE
-            rootBone.Find("wrist_l/finger_middle_meta_l/finger_middle_0_l").transform.localEulerAngles = new Vector3(0, 0, -finger3);
-            rootBone.Find("wrist_l/finger_middle_meta_l/finger_middle_0_l/finger_middle_1_l").transform.localEulerAngles = new Vector3(0, 0, -finger2);
+        if (bIndexColliding)
+        {
+            Debug.Log("COLLIDING!!");
+            //port.Write("h;");
+            indexFingerTrigger[0].GetComponent<showCollision>().colliding = false;
+        }
 
+        // Get all of the pickupObjects
+        var pickupObjects = GameObject.FindGameObjectsWithTag("PickupObject");
+        var pickupObject = pickupObjects[0].GetComponent<pickUp>();
+
+        for (var i = 0; i < pickupObjects.Length-1; i++)
+        {
+            if (pickupObjects[i].GetComponent<pickUp>().colliding)
+            {
+                pickupObject = pickupObjects[i].GetComponent<pickUp>();
+                break;
+            }
+
+        }
+
+        var bHandIsInGrabPose = true;
+        for (var i = 0; i < 8; i++)
+        {
+            if (fingerArray[i] < 70)
+            {
+                bHandIsInGrabPose = false;
+            }
+        }
+
+        var grabTrigger = bHandIsInGrabPose;
+        if (grabTrigger)
+        {
+            //Debug.Log("GRAB");
+        }
+
+        if (pickupObject.colliding && !itemGrabbed && grabTrigger)
+        {
+            itemGrabbed = true;
+            itemOldOrientation = pickupObject.transform.localRotation;
+        }
+
+        if (itemGrabbed && !grabTrigger)
+        {
+            itemGrabbed = false;
+        }
+
+        if (itemGrabbed)
+        {
+            pickupObject.transform.position = transform.position;
+            pickupObject.transform.rotation = transform.rotation;
         }
     }
 }
